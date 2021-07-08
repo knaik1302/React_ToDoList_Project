461a497be8d28ac100cfc354dcb03013ea5a23fe
Kartik V Naik <85819372+knaik1302@users.noreply.github.com>
2021-07-09 01:35:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
 import './App.css';
-import FetchRandomUser from "./Components/FetchRandomUser";
-import HookUsestate from "./HookTuts/HookUsestate";
-import HookArray from "./HookTuts/HookArray";
-import HookObject from "./HookTuts/HookObject";
-import Hookform from "./HookTuts/Hookform";
-import Useefftect from "./HookTuts/Useefftect";
 import Home from "./Todolist/Home";
 
 function App() {
   return (
-    // <HookUsestate/>
-    // <HookArray/>
-    // <HookObject/>
-    // <Hookform/>
-    // <Useefftect/>
     <Home/>
   );
 }
